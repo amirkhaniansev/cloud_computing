6864bbbfc0400df5082899dc4cc7b73c69348cfd
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2021-03-30 00:01:41 +0400

    added EF Core model configurations

---
+++ InformationCenter/Livescore/LivescoreDAL/Configurations/CompetitionConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using LivescoreDAL.Constants;
+using LivescoreDAL.Models;
+
+namespace LivescoreDAL.Configurations
+{
+    internal class CompetitionConfiguration : IEntityTypeConfiguration<Competition>
+    {
+        public void Configure(EntityTypeBuilder<Competition> builder)
+        {
+            builder.ToTable(Tables.Competition);
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id).ValueGeneratedOnAdd();
+        }
+    }
+}
---
+++ InformationCenter/Livescore/LivescoreDAL/Configurations/SeasonTeamConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using LivescoreDAL.Constants;
+using LivescoreDAL.Models;
+
+namespace LivescoreDAL.Configurations
+{
+    internal class SeasonTeamConfiguration : IEntityTypeConfiguration<SeasonTeam>
+    {
+        public void Configure(EntityTypeBuilder<SeasonTeam> builder)
+        {
+            builder.ToTable(Tables.SeasonTeam);
+            builder.HasKey(st => new
+            {
+                st.SeasonId,
+                st.TeamId
+            });
+        }
+    }
+}
